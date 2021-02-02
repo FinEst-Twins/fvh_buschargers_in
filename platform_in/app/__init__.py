b7d2d8f2bacd1caf6a767e33afe04ad40dda88c4
Sheena Puthanpurayil <sheena24789@gmail.com>
2021-02-02 15:47:35 +0200

    to_aiven

--- platform_in/app/__init__.py
+++ platform_in/app/__init__.py
@@ -7,12 +7,12 @@ import logging
 from flask import jsonify, request
 import json
 from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro import AvroProducer, Producer
 import xmltodict
 import certifi
 
 logging.basicConfig(level=logging.INFO)
-elastic_apm = ElasticAPM()
+#elastic_apm = ElasticAPM()
 # print(app.config, file=sys.stderr)
 
 success_response_object = {"status":"success"}
@@ -53,10 +53,25 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     # set up extensions
-    elastic_apm.init_app(app)
+    #elastic_apm.init_app(app)
 
-    value_schema = avro.load("avro/vehiclecharging.avsc")
-    avroProducer = AvroProducer(
+    # value_schema = avro.load("avro/vehiclecharging.avsc")
+    # avroProducer = AvroProducer(
+    #     {
+    #         "bootstrap.servers": app.config["KAFKA_BROKERS"],
+    #         "security.protocol": app.config["SECURITY_PROTOCOL"],
+    #         "sasl.mechanism": app.config["SASL_MECHANISM"],
+    #         "sasl.username": app.config["SASL_UNAME"],
+    #         "sasl.password": app.config["SASL_PASSWORD"],
+    #         "ssl.ca.location": certifi.where(),
+    #         #"debug": "security,cgrp,fetch,topic,broker,protocol",
+    #         "on_delivery": delivery_report,
+    #         "schema.registry.url": app.config["SCHEMA_REGISTRY_URL"] ,
+    #     },
+    #     default_value_schema=value_schema,
+    # )
+
+    producer = Producer(
         {
             "bootstrap.servers": app.config["KAFKA_BROKERS"],
             "security.protocol": app.config["SECURITY_PROTOCOL"],
@@ -65,10 +80,8 @@ def create_app(script_info=None):
             "sasl.password": app.config["SASL_PASSWORD"],
             "ssl.ca.location": certifi.where(),
             #"debug": "security,cgrp,fetch,topic,broker,protocol",
-            "on_delivery": delivery_report,
-            "schema.registry.url": app.config["SCHEMA_REGISTRY_URL"] ,
-        },
-        default_value_schema=value_schema,
+            "on_delivery": delivery_report
+        }
     )
 
     # shell context for flask cli
@@ -80,7 +93,7 @@ def create_app(script_info=None):
     def hello_world():
         return jsonify(health="ok")
 
-    @app.route('/ocpp/v16/observations', methods=['POST'])
+    @app.route("/ocpp/v16/observations", methods=["POST"])
     def post_vehiclecharge_data():
         try:
             data = request.get_data()
@@ -88,12 +101,19 @@ def create_app(script_info=None):
 
             # topic =
             # kafka_avro_produce(avroProducer, topic, data_dict)
+            producer.poll(0)
+
+        # Asynchronously produce a message, the delivery report callback
+        # will be triggered from poll() above, or flush() below, when the message has
+        # been successfully delivered or failed permanently.
+            producer.produce('test.finest.rawdata.vehiclecharging.ocpp', data, callback=delivery_report)
+
             return success_response_object,success_code
 
         except Exception as e:
-            avroProducer.flush()
+            producer.flush()
             logging.error("post data error", e)
-            elastic_apm.capture_exception()
+            #elastic_apm.capture_exception()
 
     return app
 
